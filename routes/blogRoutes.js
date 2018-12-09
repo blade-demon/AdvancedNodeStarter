38add36d6a260e049fe3d99b4a2d4716f56e1ad9
blade-demon <xuziwei89@gmail.com>
2018-12-09 23:54:25 +0800

    add cache for system

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,13 +13,13 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache();
 
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
---
+++ services/cache.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = async function() {
+  this.useCache = true;
+  return this;
+};
+
+mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
+  // return that
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // Otherwise, issue that query and store the result in redis
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result), "EX", 10);
+  return result;
+};
